208f3ecfdf2f48573df63b6c2388c45947ac18c3
lishijiee <2424782003@qq.com>
2022-11-23 20:32:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+技术栈：  
+spring-boot  
+
+仓库管理工具  
+maven  
---
+++ src/main/java/com/lishijiee/demosboot/DemosBootApplication.java
@@ -0,0 +1,13 @@
+package com.lishijiee.demosboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemosBootApplication.class, args);
+    }
+
+}
